13750621396d0a1ebaa3de3e914def28c8ba348b
Giuseppe Luongo <giuseppe.luongo.dev@gmail.com>
2024-01-20 13:08:40 +0100

    Integration Api: - Added get with axios to fetch data; - Added logic to handle the card list; - Added props to card and card list;

--- veganly-app/src/components/Card.jsx
+++ veganly-app/src/components/Card.jsx
@@ -1,13 +1,14 @@
-function Card(){
-    return (
-        <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
-            <img src="" alt="Recipe Image" className="w-full" />
-            <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">Title</div>
-                <p className="text-gray-700 text-base">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptates, facere corporis! Culpa, sed perferendis nesciunt sit impedit illum a obcaecati error hic, ut dolor at possimus magnam reiciendis voluptatum officia.</p>
-            </div>
+function Card({ image, title }) {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+      <img src={image} alt="Recipe Image" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="mb-2 ">
+          <h3 className="font-bold text-xl text-blue-600">{title}</h3>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- veganly-app/src/components/CardList.jsx
+++ veganly-app/src/components/CardList.jsx
@@ -1,13 +1,55 @@
-import { useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import Card from "./Card";
+import axios from "axios";
 
-function CardList() {
-    const [recipes, setRecipes] = useState([0, 1, 2]);
-    return (
-        <div className="grid gap-4 grid-cols-3 mt-8">
-            {recipes.map((recipe) => <Card key={recipe}></Card>)}
-        </div>
-        );
+const initalState = {
+  data: [],
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SUCCESS":
+      return {
+        data: action.payload,
+      };
+
+    case "ERROR":
+      return {
+        data: [],
+      };
+
+    default:
+      return state;
+  }
+};
+
+function CardList({ query }) {
+  const [recipesRedux, dispatch] = useReducer(reducer, initalState);
+  useEffect(() => {
+    axios
+      .get("https://api.spoonacular.com/recipes/complexSearch", {
+        headers: {
+          "x-api-key": "23ce25ffee0b442d8784358d06586e39",
+        },
+        params: {
+          query: query,
+          diet: "vegan",
+        },
+      })
+      .then((res) => {
+        dispatch({ type: "SUCCESS", payload: res.data.results });
+      })
+      .catch((err) => {
+        dispatch({ type: "ERROR" });
+      });
+  }, []);
+  return (
+    <div className="grid gap-4 grid-cols-3 mt-8">
+      {recipesRedux.data.map((recipe) => (
+        <Card key={recipe.id} image={recipe.image} title={recipe.title}></Card>
+      ))}
+    </div>
+  );
 }
 
 export default CardList;
